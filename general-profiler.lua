ff992d16bd4a575593d9796e3ae3120801d1c5a7
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-10-15 16:21:23 -0400

    DEVELOPING - Split models definition from profiler

--- general-profiler.lua
+++ general-profiler.lua
@@ -8,32 +8,18 @@
 require 'nn'
 require 'src/buildModel'
 require 'src/profileNet'
+require 'pl'
 
 -- Options ---------------------------------------------------------------------
+opt = lapp [[
+ -n, --net (string) Network to profile (VGG-D,Kriz)
+]]
 torch.setdefaulttensortype('torch.FloatTensor')
 
---------------------------------------------------------------------------------
--- ILSVRC 2014 classification winner
--- VGG, single network D
-name = 'VGG-D'
---------------------------------------------------------------------------------
--- Very Deep Convolutional Networks for Large-Scale Image Recognition
--- http://arxiv.org/abs/1409.1556
---------------------------------------------------------------------------------
-
--- Network parameters ----------------------------------------------------------
-nFeatureMaps = {64,64,128,128,256,256,256,512,512,512,512,512,512}
-filterSize   = { 3, 3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3}
-convPadding  = { 1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}
-convStride   = { 1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}
-poolSize     = { 1, 2,  1,  2,  1,  1,  2,  1,  1,  2,  1,  1,  2}
-poolStride   = { 1, 2,  1,  2,  1,  1,  2,  1,  1,  2,  1,  1,  2}
-hiddenUnits  = {4096, 4096, 1000}
-
--- Set colour input
-mapSize         = {}
-mapSize[0]      = {224, 224}
-nFeatureMaps[0] = 3
+-- Get model -------------------------------------------------------------------
+if opt.net == 'VGG-D' then require 'models/VGG-D'
+elseif opt.net == 'Kriz' then require 'models/Kriz'
+else error('Unknown model') end
 
 -- Building model --------------------------------------------------------------
 model = buildModel(name, nFeatureMaps, filterSize, convPadding, convStride,
---
+++ models/Kriz.lua
@@ -0,0 +1,22 @@
+--------------------------------------------------------------------------------
+-- ILSVRC 2012 classification winner
+-- Krizhevsky, double columns
+name = 'Krizhevsky'
+--------------------------------------------------------------------------------
+-- ImageNet Classification with Deep Convolutional Neural Networks
+-- http://www.cs.toronto.edu/~fritz/absps/imagenet.pdf
+--------------------------------------------------------------------------------
+
+-- Network parameters ----------------------------------------------------------
+nFeatureMaps = {96, 256, 384, 384, 256}
+filterSize   = {11,   5,   3,   3,   3}
+convPadding  = { 0,   0,   0,   0,   0}
+convStride   = { 4,   1,   1,   1,   1}
+poolSize     = { 3,   3,   1,   1,   3}
+poolStride   = { 2,   2,   1,   1,   2}
+hiddenUnits  = {4096, 4096, 1000}
+
+-- Set colour input
+mapSize         = {}
+mapSize[0]      = {224, 224}
+nFeatureMaps[0] = 3
