94be08d1df655a1ccccde8e11eca250cb934b2f7
Berin Martini <berin.martini@gmail.com>
2015-01-22 11:27:51 -0500

    Add TeraDeep Net Small into 'nets' dir
    Taken from:
    
    https://github.com/teradeep/master-net-train/tree/master/models/td-net-small.lua
    
    at commit:
    0214c421502e4371375737f2f524b618976d9af4
    
    The last 'nn.Linear' output has been hard coded to 17.

---
+++ nets/td-net-small.lua
@@ -0,0 +1,38 @@
+return {
+   name = 'TeraDeep Net Small',
+   channel = 3,
+   eye = 231,
+   mknet = function(self)
+      local model = nn.Sequential()
+
+      -- convolution layer
+      model:add(nn.SpatialConvolutionMM(3, 48, 9, 9, 4, 4, 4))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialConvolutionMM(48, 64, 5, 5, 1, 1))
+      model:add(nn.SpatialMaxPooling(2,2,2,2))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialConvolutionMM(64, 64, 3, 3, 1, 1))
+      model:add(nn.SpatialMaxPooling(2,2,2,2))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialConvolutionMM(64, 64, 3, 3, 1, 1))
+      model:add(nn.SpatialMaxPooling(2,2,2,2))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialConvolutionMM(64, 32, 3, 3, 1, 1))
+      model:add(nn.ReLU())
+
+      -- fully connected layer
+      model:add(nn.Reshape(32*3*3))
+      --model:add(nn.View(32*3*3))
+      model:add(nn.Dropout(0.5))
+      model:add(nn.Linear(32*3*3, 128))
+      model:add(nn.ReLU())
+      model:add(nn.Dropout(0.5))
+      model:add(nn.Linear(128, 128))
+      model:add(nn.ReLU())
+      model:add(nn.Dropout(0.5))
+      model:add(nn.Linear(128, 17))
+      model:add(nn.LogSoftMax())
+
+      return model
+   end
+}
