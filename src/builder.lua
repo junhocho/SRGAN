96a7abfd2b2b95dad43a3a866c43ecb773201bdf
Berin Martini <berin.martini@gmail.com>
2015-01-24 14:37:15 -0500

    Change function syntax where bracket on new line
    For some reason when used on the ARM platforms it is ambiguous and throws an
    error.

--- src/builder.lua
+++ src/builder.lua
@@ -39,8 +39,8 @@ local function generate_conv(layer, net, input)
       ..conv_s..', '..conv_s..' , '
       ..conv_p..'))')
 
-   net:add( nn.SpatialConvolutionMM
-      ( input,  output
+   net:add( nn.SpatialConvolutionMM (
+        input,  output
       , conv_k, conv_k
       , conv_s, conv_s
       , conv_p
@@ -66,8 +66,8 @@ local function generate_conv(layer, net, input)
       print('net:add(nn.SpatialMaxPooling('
          ..size..', '..size..', '..stride..', '..stride..'))')
 
-      net:add( nn.SpatialMaxPooling
-         ( size,   size
+      net:add( nn.SpatialMaxPooling (
+           size,   size
          , stride, stride
          )
       )
