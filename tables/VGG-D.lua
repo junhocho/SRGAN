f908e8c2e74a4db7ec362b3ea918fa7cc9017845
Berin Martini <berin.martini@gmail.com>
2015-04-08 09:03:09 -0700

    Decompose VGG-D network as table.insert
    Makes it easier to add and remove parts of the network.

--- tables/VGG-D.lua
+++ tables/VGG-D.lua
@@ -1,110 +1,125 @@
 return {
    name = 'VGG-D',
    channel = 3,
-   def = {
-      [1] = {
-         [1] = {
-            output = 64,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [2] = {
-            output = 64,
-            conv = { k = 3, p = 1 },
-            pool = 2,
-            nlmp = 'ReLU',
-         },
-         [3] = {
-            output = 128,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [4] = {
-            output = 128,
-            conv = { k = 3, p = 1 },
-            pool = 2,
-            nlmp = 'ReLU',
-         },
-         [5] = {
-            output = 256,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [6] = {
-            output = 256,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [7] = {
-            output = 256,
-            conv = { k = 3, p = 1 },
-            pool = 2,
-            nlmp = 'ReLU',
-         },
-         [8] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [9] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [10] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 2,
-            nlmp = 'ReLU',
-         },
-         [11] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [12] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 1,
-            nlmp = 'ReLU',
-         },
-         [13] = {
-            output = 512,
-            conv = { k = 3, p = 1 },
-            pool = 2,
-            nlmp = 'ReLU',
-         },
-      },
-      [2] = {
-         [1] = {
-            transform = {
-               name = 'Reshape',
-               size = 7,
-            },
-         },
-         [2] = {
-            linear = 4096,
-            nlmp = 'ReLU',
-         },
-         [3] = {
-            linear = 4096,
-            nlmp = 'ReLU',
-         },
-         [4] = {
-            linear = 1000,
-            nlmp = 'LogSoftMax',
-         },
-      },
-   },
+   def = {},
+
    mknet = function(self)
       local build = assert(require('src/builder'))
+
+      table.insert(self.def, {
+         output = 64,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 64,
+         conv = { k = 3, p = 1 },
+         pool = 2,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 128,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 128,
+         conv = { k = 3, p = 1 },
+         pool = 2,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 256,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+
+      table.insert(self.def, {
+         output = 256,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 256,
+         conv = { k = 3, p = 1 },
+         pool = 2,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 2,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 1,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         output = 512,
+         conv = { k = 3, p = 1 },
+         pool = 2,
+         nlmp = 'ReLU',
+      })
+---[[
+      table.insert(self.def, {
+         transform = {
+            name = 'Reshape',
+            size = 7,
+         },
+      })
+
+      table.insert(self.def, {
+         linear = 4096,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         linear = 4096,
+         nlmp = 'ReLU',
+      })
+
+      table.insert(self.def, {
+         linear = 1000,
+         nlmp = 'LogSoftMax',
+      })
+--]]
       local net, eye = build:cpu(self)
       self.eye = eye
 
