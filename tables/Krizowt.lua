e95cfdf1e7dd2ab9a06eaf878626a6564f31910b
Berin Martini <berin.martini@gmail.com>
2015-11-05 17:27:37 -0500

    Update Krizowt.lua layer one padding 3->2
    Also made stride and padding values explicit for each conv layer.

--- tables/Krizowt.lua
+++ tables/Krizowt.lua
@@ -1,36 +1,38 @@
 -- model from:
 -- https://github.com/soumith/imagenet-multiGPU.torch/blob/master/models/alexnetowt_cudnn.lua
 
+
 return {
    name = 'Krizhevsky',
    channel = 3,
+   eye = 224,
    def = {
       [1] = {
          [1] = {
             output = 64,
-            conv = { k = 11, p = 3, s = 4, },
+            conv = { k = 11, s = 4, p = 2, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [2] = {
             output = 192,
-            conv = { k = 5, p = 2, },
+            conv = { k = 5, s = 1, p = 2, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [3] = {
             output = 384,
-            conv = { k = 3, p = 1, },
+            conv = { k = 3, s = 1, p = 1, },
             nlmp = 'ReLU',
          },
          [4] = {
             output = 256,
-            conv = { k = 3, p = 1, },
+            conv = { k = 3, s = 1, p = 1, },
             nlmp = 'ReLU',
          },
          [5] = {
             output = 256,
-            conv = { k = 3, p = 1, },
+            conv = { k = 3, s = 1, p = 1, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
@@ -59,7 +61,7 @@ return {
    mknet = function(self)
       local build = assert(require('src/builder'))
       local net, eye = build:cpu(self)
-      self.eye = eye
+      self.eye = self.eye or eye
 
       return net
    end
