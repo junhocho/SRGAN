6d471e1ea0f0771e69ba2b32865b827a07cef1e7
Berin Martini <berin.martini@gmail.com>
2015-11-05 17:04:05 -0500

    Bug fix: Kriz.lua definition
    A layer is defined as split into columns if the "input" is split in 2 not if
    the output is split. Also the "output" layer entry is currently the COMBINED
    output of all columns, not the output on one column.
    
    Using the original paper as the source of the numbers:
    
    http://www.cs.toronto.edu/~fritz/absps/imagenet.pdf
    
    Layer 1) This layer does not have a split input (as its RBG) and produces 96
             outputs.
    
    Layer 2) The inputs are split into two groups of 48 maps. Each group is
    	 processed separately to produce 128 outputs making a total of 256
    	 outputs COMBINED. Thus it has 2 columns with an 256 output.
    
    Layer 3) Is a mixing layer where the 256 inputs from layer 2 produce 384
             outputs. This layer is thus not split into columns.
    
    Layer 4) The inputs are split into two groups of 192 maps. Each group is
    	 processed separately to produce 192 outputs making a total of 384
    	 outputs COMBINED. Thus it has 2 columns with an 384 output.
    
    Layer 5) The inputs are split into two groups of 192 maps. Each group is
    	 processed separately to produce 128 outputs making a total of 256
    	 outputs COMBINED. Thus it has 2 columns with an 256 output.

--- tables/Kriz.lua
+++ tables/Kriz.lua
@@ -4,36 +4,36 @@
 return {
    name = 'Krizhevsky',
    channel = 3,
+   eye = 224,
    def = {
       [1] = {
          [1] = {
-            columns = 2,
-            output = 48,
+            output = 96,
             conv = { k = 11, p = 3, s = 4, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [2] = {
             columns = 2,
-            output = 128,
+            output = 256,
             conv = { k = 5, p = 2, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [3] = {
-            output = 192,
+            output = 384,
             conv = { k = 3, p = 1, },
             nlmp = 'ReLU',
          },
          [4] = {
             columns = 2,
-            output = 192,
+            output = 384,
             conv = { k = 3, p = 1, },
             nlmp = 'ReLU',
          },
          [5] = {
             columns = 2,
-            output = 128,
+            output = 256,
             conv = { k = 3, p = 1, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
@@ -63,7 +63,7 @@ return {
    mknet = function(self)
       local build = assert(require('src/builder'))
       local net, eye = build:cpu(self)
-      self.eye = eye
+      self.eye = self.eye or eye
 
       return net
    end
