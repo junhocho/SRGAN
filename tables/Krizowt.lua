62bc34ee251779b8d3b02ece83b05f965488b21e
Eugenio <culurciello@gmail.com>
2015-11-05 15:25:53 -0600

    updated Krizewsky models
    both original 2columns and also oenweird trick from Soumith trainin script definition

--- tables/Krizowt.lua
+++ tables/Krizowt.lua
@@ -4,33 +4,29 @@ return {
    def = {
       [1] = {
          [1] = {
-            columns = 2,
-            output = 48,
+            output = 64,
             conv = { k = 11, p = 3, s = 4, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [2] = {
-            columns = 2,
-            output = 128,
+            output = 192,
             conv = { k = 5, p = 2, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
          },
          [3] = {
-            output = 192,
+            output = 384,
             conv = { k = 3, p = 1, },
             nlmp = 'ReLU',
          },
          [4] = {
-            columns = 2,
-            output = 192,
+            output = 256,
             conv = { k = 3, p = 1, },
             nlmp = 'ReLU',
          },
          [5] = {
-            columns = 2,
-            output = 128,
+            output = 256,
             conv = { k = 3, p = 1, },
             pool = { size = 3, stride = 2, },
             nlmp = 'ReLU',
